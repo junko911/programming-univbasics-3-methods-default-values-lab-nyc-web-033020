722e18bbf27c5470fc089620c8dd2468c84091b9
Junko Tahara <junkotahara911@gmail.com>
2020-03-13 20:07:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,4 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}"
-  if (veg1 == "carrots" $$ veg2 == "string beans" $$ protein == "lentils")
-    puts ""
 end
